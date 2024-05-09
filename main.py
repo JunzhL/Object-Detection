2c41de8d4604926840bcc7d6323cf908301c29a4
ljz <144968030+JunzhL@users.noreply.github.com>
2024-05-09 16:48:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-def get_predictions(pred,threshold=0.8,objects=None ):
+# get the predictions of objects in the image
+def get_predictions(pred, threshold=0.8, objects=None):
     
     predicted_classes= [(COCO_INSTANCE_CATEGORY_NAMES[i],p,[(box[0], box[1]), (box[2], box[3])]) for i,p,box in zip(list(pred[0]['labels'].numpy()),pred[0]['scores'].detach().numpy(),list(pred[0]['boxes'].detach().numpy()))]
     predicted_classes=[  stuff  for stuff in predicted_classes  if stuff[1]>threshold ]
@@ -16,12 +17,14 @@ def get_predictions(pred,threshold=0.8,objects=None ):
     if objects  and predicted_classes :
         predicted_classes=[ (name, p, box) for name, p, box in predicted_classes if name in  objects ]
     return predicted_classes
-
+# draw the box around classified objects in the image
 def draw_box(predicted_classes,image, threshold, rect_th= 10,text_size= 3,text_th=3):
-
+    # Image preprocessing
     img=(np.clip(cv2.cvtColor(np.clip(image.numpy().transpose((1, 2, 0)),0,1), cv2.COLOR_RGB2BGR),0,1)*255).astype(np.uint8).copy()
     total_number_of_each_object={}
     # print(predicted_classes)
+
+    # Draw the box around each predicted object
     for predicted_class in predicted_classes:
         # print(predicted_class)
         label=predicted_class[0]
@@ -30,14 +33,16 @@ def draw_box(predicted_classes,image, threshold, rect_th= 10,text_size= 3,text_t
         tl = tuple(map(int, box[0]))  # Convert to tuple of integers
         br = tuple(map(int, box[1]))
 
-        cv2.rectangle(img, tl, br,(0, 255, 0), rect_th) # Draw Rectangle with the coordinates
-        #cv2.putText(img,label, tl,  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) 
+        # Draw the rectangle
+        cv2.rectangle(img, tl, br,(0, 255, 0), rect_th) 
+        # Insert label text
+        cv2.putText(img,label, tl,  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) 
         cv2.putText(img, str(round(probability,2)), tl,  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,0,255),thickness=text_th)
         if label in total_number_of_each_object:
             total_number_of_each_object[label]+=1
         else:
             total_number_of_each_object[label]=1
-
+    # plot the image
     plt.figure(figsize=(10,5))
     plt.title('Object Detection with threshold: '+str(threshold))
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -65,10 +70,12 @@ def draw_box(predicted_classes,image, threshold, rect_th= 10,text_size= 3,text_t
     # plt.ylabel('Total number')
     # plt.title('Total number of each object')
     # plt.show()
-    
+
+    # free up memory
     del(img)
     del(image)
 
+# Save the RAM
 def save_RAM(image_=False):
     global image, img, pred
     torch.cuda.empty_cache()
@@ -77,10 +84,12 @@ def save_RAM(image_=False):
     if image_:
         image.close()
         del(image)
-    
+
+# Load the model
 model_ = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 model_.eval()
 
+# set parameters to False
 for name, param in model_.named_parameters():
     param.requires_grad = False
 
@@ -89,6 +98,7 @@ def model(x):
         yhat = model_(x)
     return yhat
 
+# Labels
 COCO_INSTANCE_CATEGORY_NAMES = [
     '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
     'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
@@ -109,11 +119,13 @@ COCO_INSTANCE_CATEGORY_NAMES = [
 
 # Find the total number of an object in the image above a certain threshold
 def find_total_objects(image_path, object_name=None, threshold=0.8):
+    # Load the image, preprocessing
     half = 0.5
     transform = transforms.Compose([transforms.ToTensor()])
     image = Image.open(image_path)
     image.resize([int(half * s) for s in image.size])
     img = transform(image)
+    # Find the objects in the image
     pred = model([img])
     pred_thresh=get_predictions(pred, threshold=0.8, objects=[object_name])
     draw_box(pred_thresh,img, threshold, rect_th=1, text_size=0.5, text_th=1)
@@ -121,6 +133,7 @@ def find_total_objects(image_path, object_name=None, threshold=0.8):
     save_RAM(image_=True)
 
 def find_all_objects(image_path, threshold=0.8):
+    # preprocess
     half = 0.5
     transform = transforms.Compose([transforms.ToTensor()])
     image = Image.open(image_path)
@@ -131,5 +144,6 @@ def find_all_objects(image_path, threshold=0.8):
     draw_box(pred_thresh,img, threshold, rect_th=1, text_size=0.5, text_th=1)
     del pred_thresh
     save_RAM(image_=True)
-
-find_all_objects('t3.jpg', threshold=0.01)
+# Tests
+# find_all_objects('t2.jpg', threshold=0.9)
+# find_all_objects('t3.jpg', threshold=0.4)
